278d8b8623cb810e9b4c56dcdabae46fde95b766
Laura Cabrera <lzcabrera@gmail.com>
2012-12-29 20:40:22 -0800

    add google analytics tracking code

--- includes/top.php
+++ includes/top.php
@@ -51,6 +51,21 @@
 	<link href='http://fonts.googleapis.com/css?family=Elsie+Swash+Caps:400,900' rel='stylesheet' type='text/css'>
 	<script src="/javascripts/foundation/modernizr.foundation.js"></script>
 
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-9015988-1']);
+	  _gaq.push(['_setDomainName', 'funnelcity.com']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
 </head>
 <body>
 
