243dfe5df4c818a55bf2ce916b3f357c05320d09
laura <laura@funnelcity.com>
2013-01-01 18:00:09 -0800

    add debug console to troubleshoot ipad3 orientationchange

--- includes/top.php
+++ includes/top.php
@@ -68,7 +68,20 @@
 
 </head>
 <body>
-		
+		<!-- begin: for debugging only -->
+		  <div id="fqv" style="position:fixed;top:4px;left:4px;z-index:999;color:#fff;">
+		  	<p style="font-size:12px;background:rgba(0,0,0,0.75);padding:5px;margin-bottom:1px;line-height:1.2;">
+					<span class="left">Media:</span> 
+					<span style="font-weight:bold;" class="show-for-xlarge">Extra Large</span><span style="font-weight:bold;" class="show-for-large">Large</span>
+					<span style="font-weight:bold;" class="show-for-medium">Medium</span>
+					<span style="font-weight:bold;" class="show-for-small">Small</span>
+					<span style="font-weight:bold;" class="show-for-landscape">Landscape</span>
+					<span style="font-weight:bold;" class="show-for-portrait">Portrait</span>
+					<span style="font-weight:bold;" class="show-for-touch">Touch</span>
+				</p>
+				<p id="window-width" style="background:#000"></p>
+			</div>
+		<!-- end: for debugging only -->
 
 	<div class="row">
 
